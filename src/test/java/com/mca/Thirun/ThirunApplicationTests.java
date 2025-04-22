9cd79fc83df0282f1bb282aba82bf4659200a508
ThirugnanamSethu <thirugnanamsethu1t@gmail.com>
2025-04-22 11:49:07 +0530

    Initial commit spring boot

---
+++ src/test/java/com/mca/Thirun/ThirunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mca.Thirun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
